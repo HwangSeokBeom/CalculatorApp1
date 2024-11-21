18dcebcd005c07b76baafdec5837787c5b76850d
HwangSeokBeom <tjrqja07@naver.com>
2024-11-22 01:11:40 +0900

    calculatorButton.swift 추가

--- CalculatorApp1/CalculatorButton.swift
+++ CalculatorApp1/CalculatorButton.swift
@@ -1,9 +1,10 @@
 //
-//  Operation.swift
+//  Untitled.swift
 //  CalculatorApp1
 //
-//  Created by Hwangseokbeom on 11/21/24.
+//  Created by Hwangseokbeom on 11/22/24.
 //
+
 enum CalculatorButton {
     case allClear
     case equals
@@ -11,22 +12,16 @@ enum CalculatorButton {
     case subtraction
     case multiplication
     case division
-    case number(Int) // 숫자는 Int로 처리
+    case number(Int)
 
     static func from(title: String) -> CalculatorButton? {
         switch title {
-        case "AC":
-            return .allClear
-        case "=":
-            return .equals
-        case "+":
-            return .addition
-        case "-":
-            return .subtraction
-        case "*":
-            return .multiplication
-        case "/":
-            return .division
+        case "AC": return .allClear
+        case "=": return .equals
+        case "+": return .addition
+        case "-": return .subtraction
+        case "*": return .multiplication
+        case "/": return .division
         default:
             if let number = Int(title) {
                 return .number(number)
@@ -34,15 +29,15 @@ enum CalculatorButton {
             return nil
         }
     }
-    
+
     func getOperation() -> (Int, Int) -> Int {
-           switch self {
-           case .addition: return (+)
-           case .subtraction: return (-)
-           case .multiplication: return (*)
-           case .division: return (/)
-           default:
-               fatalError("Invalid operation")
-           }
+        switch self {
+        case .addition: return (+)
+        case .subtraction: return (-)
+        case .multiplication: return (*)
+        case .division: return (/)
+        default:
+            fatalError("Invalid operation")
+        }
     }
 }
